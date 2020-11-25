8b26819bddb0b1cee5374c7ac367de30c402f5e0
matteomoisan22 <matteodev22@gmail.com>
2020-11-25 17:31:16 +0100

    controller : cleaner code

--- controller/searchByCity.php
+++ controller/searchByCity.php
@@ -14,14 +14,15 @@ if(isset($_GET['city'])) {
         $date = date("d/m/Y");
 
         for ($i = 0; $i <= 16; $i=$i+8) {
-            $data['description'] = $r->list[$i]->weather[0]->description;
-            $data['temp'] = $r->list[$i]->main->temp;
-            $data['feels_like'] = $r->list[$i]->main->feels_like;
-            $data['humidity'] = $r->list[$i]->main->humidity;
 
-            $date = $r->list[$i]->dt_txt;
+            $description = $r->list[$i]->weather[0]->description;
+            $temperature = $r->list[$i]->main->temp;
+            $feels_like = $r->list[$i]->main->feels_like;
+            $humidity = $r->list[$i]->main->humidity;
 
-            echo "<tr><th class='text-center'>".$city."</th><th class='text-center'>".$date."</th><th class='text-center'>".$data['description']."</th><th class='text-center'>".$data['temp']." °C</th><th class='text-center'>".$data['feels_like']." °C</th><th class='text-center'>".$data['humidity']." %</th></tr>";
+            $date = date('d/m/Y', strtotime($r->list[$i]->dt_txt));
+
+            echo "<tr><th class='text-center'>".$city."</th><th class='text-center'>".$date."</th><th class='text-center'>".$description."</th><th class='text-center'>".$temperature." °C</th><th class='text-center'>".$feels_like." °C</th><th class='text-center'>".$humidity." %</th></tr>";
         }
 
     } else {
